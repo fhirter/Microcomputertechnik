7160394f08e948520da4c5ee978d291565e3a29b
fhirter <fabian.hirter@edu.teko.ch>
2022-01-05 18:53:36 +0100

    feat (03_Interrupts): move Serial.print out of ISR

--- AVR/Exercises/03_Interrupts_Uebung1b/03_Interrupts_Uebung1b.ino
+++ AVR/Exercises/03_Interrupts_Uebung1b/03_Interrupts_Uebung1b.ino
@@ -10,9 +10,10 @@ void setup() {
     Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
 void loop() {
     float d = 1000;
+
+    Serial.println(state);
 
     if (state == 1) {
         digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
@@ -24,5 +25,4 @@ void loop() {
 
 void blink() {
     state = !state;
-    Serial.println(state);
 }
