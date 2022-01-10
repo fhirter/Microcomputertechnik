7962b6ffdb978a34baa65ab83f3166d342dda40d
fhirter <fabian.hirter@edu.teko.ch>
2022-01-10 17:34:30 +0100

    feat (03_Interrupts): add solutions for "Übung 3" (Debouncing)

---
+++ AVR/Exercises/03_Interrupts/03_Interrupts_Uebung3/03_Interrupts_Uebung3.ino
@@ -0,0 +1,44 @@
+#include "timer-api.h"
+
+const byte interruptPin = 2;
+
+volatile uint8_t buttonFlag = 0;
+volatile uint8_t isTimerEnabled = 0;
+
+void setup() {
+    pinMode(interruptPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), blink, FALLING);
+    Serial.begin(9600);
+}
+
+void loop() {
+    if(buttonFlag == 1) {
+        Serial.println("button pressed");
+        buttonFlag = 0;
+    }
+}
+
+// button ISR
+void blink() {
+    if(!isTimerEnabled) {
+        // initialize and start 20ms timer
+        timer_init_ISR_50Hz(TIMER_DEFAULT);
+        isTimerEnabled = 1;
+    }
+}
+
+// timer ISR
+void timer_handle_interrupts(int timer) {
+    static uint8_t buttonCounter = 0;
+    if(digitalRead(interruptPin)) {
+        buttonCounter++;
+        Serial.print(buttonCounter);
+    }
+
+    if(buttonCounter >= 5) {
+        buttonFlag = 1;
+        timer_stop_ISR(TIMER_DEFAULT);
+        isTimerEnabled = 0;
+        buttonCounter = 0;
+    }
+}
