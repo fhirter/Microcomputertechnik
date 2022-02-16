1e1d6e6f35538faafc0c52cd5ba39c905884f199
Fabian Hirter <mail@fabianhirter.ch>
2022-02-16 19:32:06 +0100

    add solutions for interrupt driven debouncing

--- ARM_M0/Exercises/03_Interrupts/03_Interrupts_Uebung3/03_Interrupts_Uebung3.ino
+++ ARM_M0/Exercises/03_Interrupts/03_Interrupts_Uebung3/03_Interrupts_Uebung3.ino
@@ -5,28 +5,34 @@ const byte interruptPin = 2;
 
 volatile uint8_t buttonFlag = 0;
 volatile uint8_t isTimerEnabled = 0;
+volatile uint8_t buttonCounter = 0;
 
 const uint16_t TIMER_INTERVAL_MS = 20;
 NRF52_MBED_Timer ITimer(NRF_TIMER_3);
 
 void setup() {
     pinMode(interruptPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), blink, FALLING);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), buttonISR, FALLING);
     Serial.begin(9600);
-}
 
-void loop() {
-    if(buttonFlag == 1) {
-        Serial.println("button pressed");
-        buttonFlag = 0;
+    while(1) {
+      if(buttonFlag == 1) {
+          Serial.print("button pressed");
+          Serial.println(digitalRead(interruptPin));
+          buttonFlag = 0;
+      }
     }
 }
 
+void loop() {
+
+}
+
 // button ISR
-void blink() {
+void buttonISR() {
     if(!isTimerEnabled) {
         // start 20ms timer
-        if(ITimer.attachInterruptInterval(TIMER_INTERVAL_MS*1000, timer_handle_interrupts)) {
+        if(ITimer.attachInterruptInterval(TIMER_INTERVAL_MS*1000, timerISR)) {
             Serial.print(F("Starting ITimer3 OK"));
         } else {
             Serial.print(F("Starting ITimer3 Failed"));
@@ -36,15 +42,14 @@ void blink() {
 }
 
 // timer ISR
-void timer_handle_interrupts() {
-    static uint8_t buttonCounter = 0;
-    if(digitalRead(interruptPin)) {
+void timerISR() {
+    if(digitalRead(interruptPin) == 0) {
         buttonCounter++;
     }
 
     if(buttonCounter >= 5) {
         buttonFlag = 1;
-        ITimer.stopTimer();
+        // disabling interrupt (detachInterrupt) is not working.
         isTimerEnabled = 0;
         buttonCounter = 0;
     }
