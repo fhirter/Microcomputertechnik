c9be0d0dc4a82a35c17eb2cc40f77a7e550d78e5
Fabian Hirter <mail@fabianhirter.ch>
2022-02-16 18:58:24 +0100

    add solutions for interrupt driven blink

--- ARM_M0/Exercises/03_Interrupts/03_Interrupts_Uebung2b/03_Interrupts_Uebung2b.ino
+++ ARM_M0/Exercises/03_Interrupts/03_Interrupts_Uebung2b/03_Interrupts_Uebung2b.ino
@@ -1,45 +1,45 @@
 #include <NRF52_MBED_TimerInterrupt.h>
 #include <NRF52_MBED_ISR_Timer.h>
 
-
-const byte interruptPin = 2;
 volatile bool enableBlinking = 1;
 volatile bool state = 1;
 
-unsigned long referenceTime = 0;
-unsigned long deltaTime = 0;
+const byte INTERRUPT_PIN = 2;
+const uint16_t TIMER_INTERVAL_MS = 2000;
 
-const uint16_t TIMER_INTERVAL_MS = 1000;
+volatile unsigned long referenceTime = micros();
+volatile unsigned long deltaTime = 0;
+
+void timerISR();
+void buttonISR();
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(interruptPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), blink, FALLING);
-    Serial.begin(9600);
-
-    referenceTime = micros();
-
     NRF52_MBED_Timer ITimer(NRF_TIMER_3);
-    if(ITimer.attachInterruptInterval(TIMER_INTERVAL_MS*1000, timer_handle_interrupts)) {
+    if(ITimer.attachInterruptInterval(TIMER_INTERVAL_MS*1000, timerISR)) {
         Serial.print(F("Starting ITimer3 OK"));
     } else {
         Serial.print(F("Starting ITimer3 Failed"));
     }
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), buttonISR, FALLING);
+
+    while(1) {
+        Serial.println(deltaTime);
+    }
 }
 
 void loop() {
-    delay(TIMER_INTERVAL_MS);     // deltaTime does not depend on this delay
 
-    Serial.println(deltaTime, DEC);
 }
 
-// button ISR
-void blink() {
+void buttonISR() {
     enableBlinking = !enableBlinking;
 }
 
-// timer ISR
-void timer_handle_interrupts() {
+void timerISR() {
     unsigned long currentTime = micros();
     deltaTime = currentTime - referenceTime;
 
