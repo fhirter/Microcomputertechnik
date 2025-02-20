67102f7219b1ed614d86a28e98f2cfe3790417ea
Fabian Hirter <mail@fabianhirter.ch>
2025-02-20 17:48:25 +0100

    implement decoder (wip)

--- Exercises/VirtualMachine/Solution/Decoder.go
+++ Exercises/VirtualMachine/Solution/Decoder.go
@@ -1,5 +1,9 @@
 package main
 
-func decodeInstruction(instruction InstructionSize) {
+func decodeInstruction(instruction InstructionSize) opCode {
+	var opcode = instruction & 0b111111         // 0 - 6
+	var funct3 = instruction & (0b111 << 12)    // 12 - 14
+	var func7 = instruction & (0b1111111 << 25) // 25 - 31
 
+	return opCode(opcode)
 }
--- Exercises/VirtualMachine/Solution/Decoder_test.go
+++ Exercises/VirtualMachine/Solution/Decoder_test.go
@@ -2,7 +2,13 @@ package main
 
 import "testing"
 
-func TestDecode(t *testing.T) {
-	var instruction InstructionSize = 0b0
-	decodeInstruction(instruction)
+func TestDecodeAdd(t *testing.T) {
+	var instruction InstructionSize = 0b....
+	var operation = decodeInstruction(instruction)
+
+	var expectedOperation opCode = OpCodeValues.add
+
+	if operation != expectedOperation {
+		t.Fatalf(`ADD is not decoded correctly. given: %b, expected: %b`, operation, expectedOperation)
+	}
 }
